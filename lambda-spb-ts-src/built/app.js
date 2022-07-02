b259cefa22f5f39bf865a2f2ffe3981ec21fb0dc
Luiz Felipph Calado <luizfelipph@gmail.com>
2022-07-02 20:35:46 -0300

    montando o lambda

--- lambda-spb-ts-src/built/app.js
+++ lambda-spb-ts-src/built/app.js
@@ -1,37 +1,54 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
 const SPBHeader_1 = require("./service/spb/model/SPBHeader");
-const FileUtils_1 = require("./utils/FileUtils");
-const SPBProtocolV2_1 = require("./service/spb/impl/SPBProtocolV2");
-async function main() {
-    const filePath = '../req.gz.dat';
-    const fd = fs.openSync(filePath, 'r'); // file descriptor
-    const stats = fs.statSync(filePath); // file details
-    var sharedBuffer = Buffer.alloc(SPBHeader_1.SPBHeader.getHeaderSize());
-    //carregando somente o header    
-    await FileUtils_1.FileUtils.readBytes(fd, sharedBuffer);
-    var header = new SPBHeader_1.SPBHeader(sharedBuffer);
+const AWS = require("aws-sdk");
+exports.lambdaHandler = async (event, context) => {
+    const s3 = new AWS.S3();
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    var headerContent;
+    await s3.getObject({ Bucket: bucket, Key: key, Range: '0-587' }, (err, data) => {
+        if (!err) {
+            headerContent = data.Body;
+        }
+    }).promise();
+    console.log(headerContent);
+    var header = new SPBHeader_1.SPBHeader(headerContent);
     console.log(header);
-    var contentSize = stats.size - SPBHeader_1.SPBHeader.getHeaderSize();
-    sharedBuffer = Buffer.alloc(contentSize);
-    await FileUtils_1.FileUtils.readBytes(fd, sharedBuffer, 588);
-    const protocol = new SPBProtocolV2_1.SPBProtocolV2(sharedBuffer, header);
-    var publicKey = fs.readFileSync("./certDestino/publicKeyDestino.pem");
-    var privateKey = fs.readFileSync("./certDestino/privateKeyDestino.pem");
-    const symetricKey = protocol.decryptSymetricKey(privateKey);
-    console.log(symetricKey.toString('utf-8'));
-    protocol.decryptContents();
-    var publicKeyOrigem = fs.readFileSync("./certOrigem/publicKeyOrigem.pem");
-    if (!protocol.checkSignature(publicKeyOrigem)) {
-        console.log("Assinatura inválida!");
-    }
-    console.log(protocol.unpack());
-}
-main().then((ok) => {
-    const used = process.memoryUsage();
-    for (let key in used) {
-        console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100)} KB`);
-    }
-});
+    return {
+        statusCode: 200,
+        body: `Recebido: ${key}`
+    };
+};
+exports.lambdaHandler();
+// async function main() {
+//     const filePath = '../req.gz.dat';
+//     const fd = fs.openSync(filePath, 'r'); // file descriptor
+//     const stats = fs.statSync(filePath); // file details
+//     var sharedBuffer = Buffer.alloc(SPBHeader.getHeaderSize());
+//     //carregando somente o header    
+//     await FileUtils.readBytes(fd, sharedBuffer);
+//     var header = new SPBHeader(sharedBuffer);
+//     console.log(header);
+//     var contentSize = stats.size - SPBHeader.getHeaderSize();
+//     sharedBuffer = Buffer.alloc(contentSize);
+//     await FileUtils.readBytes(fd, sharedBuffer, 588);
+//     const protocol = new SPBProtocolV2(sharedBuffer, header);
+//     var publicKey = fs.readFileSync("./certDestino/publicKeyDestino.pem");
+//     var privateKey = fs.readFileSync("./certDestino/privateKeyDestino.pem");
+//     const symetricKey = protocol.decryptSymetricKey(privateKey);
+//     console.log(symetricKey.toString('utf-8'))
+//     protocol.decryptContents();
+//     var publicKeyOrigem = fs.readFileSync("./certOrigem/publicKeyOrigem.pem");
+//     if (!protocol.checkSignature(publicKeyOrigem)) {
+//         console.log("Assinatura inválida!");
+//     }
+//     console.log(protocol.unpack());
+// }
+// main().then((ok) => {
+//     const used = process.memoryUsage();
+//     for (let key in used) {
+//         console.log(`${key} ${Math.round(used[key as keyof typeof used] / 1024 / 1024 * 100)} KB`);
+//     }
+// });
 //# sourceMappingURL=app.js.map
--- lambda-spb-ts-src/src/app.ts
+++ lambda-spb-ts-src/src/app.ts
@@ -3,43 +3,72 @@ import { SPBHeader } from './service/spb/model/SPBHeader';
 import { FileUtils } from './utils/FileUtils';
 import { SPBProtocolV2 } from './service/spb/impl/SPBProtocolV2';
 
-async function main() {
+import {S3CreateEvent} from "aws-lambda";
+import * as AWS from "aws-sdk";
 
-    const filePath = '../req.gz.dat';
-    const fd = fs.openSync(filePath, 'r'); // file descriptor
-    const stats = fs.statSync(filePath); // file details
 
-    var sharedBuffer = Buffer.alloc(SPBHeader.getHeaderSize());
-    //carregando somente o header    
-    await FileUtils.readBytes(fd, sharedBuffer);
-    var header = new SPBHeader(sharedBuffer);
-    console.log(header);
+exports.lambdaHandler = async (event:S3CreateEvent, context:any) => {
 
-    var contentSize = stats.size - SPBHeader.getHeaderSize();
-    sharedBuffer = Buffer.alloc(contentSize);
+    const s3 = new AWS.S3();
+    
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    var headerContent:Buffer;
+    await s3.getObject({ Bucket: bucket, Key: key, Range: '0-587' }, (err,data) => {
+        if(!err) {
+            headerContent = data.Body as Buffer;
+        }
+    }).promise();
 
-    await FileUtils.readBytes(fd, sharedBuffer, 588);
-    const protocol = new SPBProtocolV2(sharedBuffer, header);
+    console.log(headerContent);
+    
 
-    var publicKey = fs.readFileSync("./certDestino/publicKeyDestino.pem");
+    var header = new SPBHeader(headerContent);
+    console.log(header);    
 
-    var privateKey = fs.readFileSync("./certDestino/privateKeyDestino.pem");
-
-    const symetricKey = protocol.decryptSymetricKey(privateKey);
-    console.log(symetricKey.toString('utf-8'))
-
-    protocol.decryptContents();
-    var publicKeyOrigem = fs.readFileSync("./certOrigem/publicKeyOrigem.pem");
-    if (!protocol.checkSignature(publicKeyOrigem)) {
-        console.log("Assinatura inválida!");
+    return {
+        statusCode: 200,
+        body: `Recebido: ${key}`
     }
-    console.log(protocol.unpack());
-}
+};
 
-main().then((ok) => {
-    const used = process.memoryUsage();
-    for (let key in used) {
-        console.log(`${key} ${Math.round(used[key as keyof typeof used] / 1024 / 1024 * 100)} KB`);
-    }
-});
+// async function main() {
+
+//     const filePath = '../req.gz.dat';
+//     const fd = fs.openSync(filePath, 'r'); // file descriptor
+//     const stats = fs.statSync(filePath); // file details
+
+//     var sharedBuffer = Buffer.alloc(SPBHeader.getHeaderSize());
+//     //carregando somente o header    
+//     await FileUtils.readBytes(fd, sharedBuffer);
+//     var header = new SPBHeader(sharedBuffer);
+//     console.log(header);
+
+//     var contentSize = stats.size - SPBHeader.getHeaderSize();
+//     sharedBuffer = Buffer.alloc(contentSize);
+
+//     await FileUtils.readBytes(fd, sharedBuffer, 588);
+//     const protocol = new SPBProtocolV2(sharedBuffer, header);
+
+//     var publicKey = fs.readFileSync("./certDestino/publicKeyDestino.pem");
+
+//     var privateKey = fs.readFileSync("./certDestino/privateKeyDestino.pem");
+
+//     const symetricKey = protocol.decryptSymetricKey(privateKey);
+//     console.log(symetricKey.toString('utf-8'))
+
+//     protocol.decryptContents();
+//     var publicKeyOrigem = fs.readFileSync("./certOrigem/publicKeyOrigem.pem");
+//     if (!protocol.checkSignature(publicKeyOrigem)) {
+//         console.log("Assinatura inválida!");
+//     }
+//     console.log(protocol.unpack());
+// }
+
+// main().then((ok) => {
+//     const used = process.memoryUsage();
+//     for (let key in used) {
+//         console.log(`${key} ${Math.round(used[key as keyof typeof used] / 1024 / 1024 * 100)} KB`);
+//     }
+// });
 
